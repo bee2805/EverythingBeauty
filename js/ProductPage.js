ebd93811f4a4bd30f059f4f8666023781f4f7eeb
Bron200089 <200089@virtualwindow.co.za>
2022-03-23 10:38:47 +0200

    Initial set up and layout
    Set up the file with some layout and styling

--- js/ProductPage.js
+++ js/ProductPage.js
@@ -1,13 +1,24 @@
 $(function(){
+    // var btn = $('#button');
 
-    const benefit = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=benefit';
-    const clinique = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=clinique';
-    const dior = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=dior';
-    const maybelline = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
-    const nyx = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=nyx';
-    const revlon = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=revlon';
-    const smashbox = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=smashbox';
+    const baseURL = 'http://makeup-api.herokuapp.com/api/v1/products.json'
+    const benefit = baseURL + '?brand=benefit';
+    const clinique = baseURL + '?brand=clinique';
+    const dior = baseURL + '?brand=dior';
+    const maybelline = baseURL + '?brand=maybelline';
+    const nyx = baseURL + '?brand=nyx';
+    const revlon = baseURL + '?brand=revlon';
+    const smashbox = baseURL + '?brand=smashbox';
 
+    $.getJSON(baseURL,function(data){
+        for(i=0; i < data[i].brand; i++){
+            $(".filterOptions").append(`
+                <select name="" class="btn btn-dark justify-content-center" id="">
+                    <option value="">${data[i].brand}</option>
+                </select>
+            `)
+        };
+    })
 
     $.getJSON(benefit,function(data){
 
@@ -15,15 +26,16 @@ $(function(){
 
         for(i=0; i < data.length; i++){
             $("#benefit").append(`
-                <div class="col-lg-2 col-md-6 col-sm-12 my-2 mx-2 bg-light rounded border productCard shadow">
+                <div class="col-lg-2 col-md-4 col-sm-12 my-2 mx-2 bg-light rounded border productCard shadow">
                     <div class="justify-content-center" style="height: 200px;">
                         <img class="img-fluid img-thumbnail rounded mt-4 h-100 mx-auto d-block" src="${data[i].api_featured_image}">
                     </div>
                     </br>
-                    <h4 class="mt-2" style="font-weight: bold;">${data[i].name}</h4>
-                    <h5 style="font-weight: bold;">Brand:<h5>
-                    <p> ${data[i].brand}</p>
-                    <a href="${data[i].product_link}"><button class="btn-dark rounded align-self-baseline">Product Link</button></a>
+                    <div class="card-footer">
+                        <h5 class="mt-2" style="font-weight: bold;">${data[i].name}</h5>
+                        <p>Brand: ${data[i].brand}</p>
+                        <a href="${data[i].product_link}"><button class="align-bottom btn-dark rounded">Product Link</button></a>
+                    </div>
                 </div>
             `);
         };
@@ -36,7 +48,7 @@ $(function(){
 
         for(i=0; i < data.length; i++){
             $("#clinique").append(`
-                <div class="col-lg-2 col-md-6 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
+                <div class="col-lg-2 col-md-4 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
                     <div class="justify-content-center" style="height: 200px;">
                         <img class="img-fluid img-thumbnail rounded mt-4 h-100 mx-auto d-block" src="${data[i].api_featured_image}">
                     </div>
@@ -57,7 +69,7 @@ $(function(){
 
         for(i=0; i < data.length; i++){
             $("#dior").append(`
-                <div class="col-lg-2 col-md-6 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
+                <div class="col-lg-2 col-md-4 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
                     <div class="justify-content-center" style="height: 200px;">
                         <img class="img-fluid img-thumbnail rounded mt-4 h-100 mx-auto d-block" src="${data[i].api_featured_image}">
                     </div>
@@ -77,7 +89,7 @@ $(function(){
 
         for(i=0; i < data.length; i++){
             $("#maybelline").append(`
-                <div class="col-lg-2 col-md-6 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
+                <div class="col-lg-2 col-md-4 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
                     <div class="justify-content-center" style="height: 200px;">
                         <img class="img-fluid img-thumbnail rounded mt-4 h-100 mx-auto d-block" src="${data[i].api_featured_image}">
                     </div>
@@ -97,7 +109,7 @@ $(function(){
 
         for(i=0; i < data.length; i++){
             $("#nyx").append(`
-                <div class="col-lg-2 col-md-6 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
+                <div class="col-lg-2 col-md-4 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
                     <div class="justify-content-center" style="height: 200px;">
                         <img class="img-fluid img-thumbnail rounded mt-4 h-100 mx-auto d-block" src="${data[i].api_featured_image}">
                     </div>
@@ -117,7 +129,7 @@ $(function(){
 
         for(i=0; i < data.length; i++){
             $("#revlon").append(`
-                <div class="col-lg-2 col-md-6 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
+                <div class="col-lg-2 col-md-4 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
                     <div class="justify-content-center" style="height: 200px;">
                         <img class="img-fluid img-thumbnail rounded mt-4 h-100 mx-auto d-block" src="${data[i].api_featured_image}">
                     </div>
@@ -137,7 +149,7 @@ $(function(){
 
         for(i=0; i < data.length; i++){
             $("#smashbox").append(`
-                <div class="col-lg-2 col-md-6 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
+                <div class="col-lg-2 col-md-4 col-sm-12 my-2 mx-2 bg-light rounded border productCard">
                     <div class="justify-content-center" style="height: 200px;">
                         <img class="img-fluid img-thumbnail rounded mt-4 h-100 mx-auto d-block" src="${data[i].api_featured_image}">
                     </div>
